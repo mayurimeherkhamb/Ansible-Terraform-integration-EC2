57adb5afa6b97758abeda4153f8c7fb6e2dfdd25
mayuribhaleraomeherkhamb <104008268+mayuribhaleraomeherkhamb@users.noreply.github.com>
2024-05-04 14:44:17 +0530

    first commit

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "myec2" {
-  ami                    = "ami-08df646e18b182346"
+  ami                    = "ami-07caf09b362be10b8"
   instance_type          = "t2.micro"
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1b"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  key_name = "pswain"
+  key_name = "ansible-terraform-keypair"
 
   tags = {
     name = "testec2"
   }
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.myec2.public_ip} >> /etc/ansible/hosts"
+    command = "echo ${aws_instance.myec2.public_ip} | sudo tee -a /etc/ansible/hosts"
   }
 }
